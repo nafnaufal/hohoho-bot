379f59059640650072acac4b78904a8670501c55
Naufal Anbial Falah <75089762+nafnaufal@users.noreply.github.com>
2021-10-13 16:09:02 +0000

    add message command

--- main.py
+++ main.py
@@ -1,11 +1,24 @@
+import os
 import discord
 
 client = discord.Client()
+hohoho = os.environ['token']
+
+istighfar = ['Astagfirullah', 'astagfirullah', 'astaghfirullah', 'Astaghfirullah', 'istighfar', 'Istighfar']
 
 @client.event
 async def on_ready():
-    print("we have loggin as {0.user}'.format(client)")
+    print("we have loggin as {0.user}".format(client))
 
-@client.event()
+@client.event
 async def on_message(message):
+    if message.author == client.user:
+        return
+    for i in istighfar:
+        if message.content.startswith(i):
+            await message.channel.send('Astagfirullah Hanip')
+    if message.content.startswith('sabar'):
+            await message.channel.send('Astagfirullah sabar')
     
+    
+client.run(hohoho)
